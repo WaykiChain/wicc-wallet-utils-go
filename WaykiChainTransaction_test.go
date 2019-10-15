f4e1adfd0d0c8facd54f6ec6b47ba7684c622dde
jk <327413510@qq.com>
2019-10-15 19:59:47 +0800

    update

--- WaykiChainTransaction_test.go
+++ WaykiChainTransaction_test.go
@@ -8,126 +8,59 @@ import (
 )
 
 /*
-    * 账户注册交易,新版本已基本废弃，可改用公钥注册，免注册费用
-    * Account registration transaction, the new version has been abandoned, you can use public key registration, free registration fee
-    * Minimum fee 0.1WICC
-    * */
-func TestSignRegisterAccountTx(t *testing.T) {
-
-	privateKey := "YAa1wFCfFnZ5bt4hg9MDeDevTMd1Nu874Mn83hEXwtfAL2vkQE9f" //"Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
-	var txParam RegisterAccountTxParam
-	txParam.ValidHeight = 630314
-	txParam.Fees = 10000000
-
-	hash, err := SignRegisterAccountTx(privateKey, &txParam)
-	if err != nil {
-		t.Error("SignRegisterAccountTx err: ", err)
-	}
-	println(hash)
-}
-
-/*
-  * 转账交易
-  * common transfer
-  * fee Minimum 0.1 wicc
+  * 多币种转账交易 ,支持多种币种转账
+  * Test nUniversal Coin Transfer Tx
+  * fee Minimum 100000 sawi
   * */
-func TestSignCommonTx(t *testing.T) {
-
-	privateKey := "YAa1wFCfFnZ5bt4hg9MDeDevTMd1Nu874Mn83hEXwtfAL2vkQE9f" //"Y7V1jwCRr8D3tyPTkcsjgBTHwZN45b1U3ueZfJ5oWVJqwcKpArou"
-	var txParams CommonTxParam
-	txParams.ValidHeight = 630314
-	txParams.SrcRegId = "158-1"
-	txParams.DestAddr = "wSSbTePArv6BkDsQW9gpGCTX55AXVxVKbd"
-	txParams.Values = 10000
-	txParams.Fees = 10000000
-	txParams.PubKey = "03e93e7d870ce6f1c9997076c56fc24e6381c612662cd9a5a59294fac9ba7d21d7"
-    txParams.Memo="test transfer"
-	hash, err := SignCommonTx(privateKey, &txParams)
-	if err != nil {
-		t.Error("SignCommonTx err: ", err)
-	}
-	println(hash)
-}
-
-/*
-   * 合约调用交易
-   * Contract transaction sample
-   * fee Minimum 0.01 wicc
-   * */
-func TestSignCallContractTx(t *testing.T) {
-
-	privateKey := "Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
-
-	var txParam CallContractTxParam
-	txParam.ValidHeight = 22365
+func TestSignUCoinTransferTx(t *testing.T) {
+	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
+	var txParam UCoinTransferTxParam
+	txParam.FeeSymbol = string(commons.WICC)
+	txParam.Fees = 1000000
+	txParam.ValidHeight = 297449
 	txParam.SrcRegId = "0-1"
-	txParam.AppId = "20988-1"
-	txParam.Fees = 100000
-	txParam.Values = 1000000
+	txParam.Dests=NewDestArr()
+	dest:=Dest{string(commons.WICC),1000000, "wNDue1jHcgRSioSDL4o1AzXz3D72gCMkP6"}
+	txParam.Dests.Add(&dest)
+	txParam.PubKey = "036c5397f3227a1e209952829d249b7ad0f615e43b763ac15e3a6f52627a10df21"
+	txParam.Memo = ""
+	hash, err := SignUCoinTransferTx(privateKey, &txParam)
+	if err != nil {
+		t.Error("UCoinTransferTx err: ", err)
+	}
+	println(hash)
+}
+
+/*
+  * 多币种合约调用交易 ,支持多种币种转账
+  * Test nUniversal Coin Contract Tx
+  * fee Minimum 1000000 sawi
+  * */
+func TestSignUCoinCallContractTx(t *testing.T) {
+	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
+	var txParam UCoinContractTxParam
+	txParam.FeeSymbol = string(commons.WICC)
+	txParam.CoinSymbol = string(commons.WICC)
+	txParam.CoinAmount = 1000000
+	txParam.Fees = 1000000
+	txParam.ValidHeight = 297449
+	txParam.SrcRegId = "0-1"
+	txParam.AppId = "0-1"
+	txParam.PubKey = "036c5397f3227a1e209952829d249b7ad0f615e43b763ac15e3a6f52627a10df21"
 	txParam.ContractHex = "f017"
-	txParam.PubKey = "03e93e7d870ce6f1c9997076c56fc24e6381c612662cd9a5a59294fac9ba7d21d7"
-	hash, err := SignCallContractTx(privateKey, &txParam)
+	hash, err := SignUCoinCallContractTx(privateKey, &txParam)
 	if err != nil {
-		t.Error("SignCallContractTx err: ", err)
+		t.Error("SignUCoinCallContractTx err: ", err)
 	}
 	println(hash)
 }
 
-/*
-* 投票交易
-* Voting transaction
-* fee Minimum 0.01 wicc
-* */
-func TestSignDelegateTx(t *testing.T) {
-	privateKey := "Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
-	var txParams DelegateTxParam
-	txParams.ValidHeight = 95728
-	txParams.SrcRegId = ""
-	txParams.Fees = 1000000
-	txParams.PubKey = "03e93e7d870ce6f1c9997076c56fc24e6381c612662cd9a5a59294fac9ba7d21d7"
-	txParams.Votes = NewOperVoteFunds()
-	pubKey, _ := hex.DecodeString("025a37cb6ec9f63bb17e562865e006f0bafa9afbd8a846bd87fc8ff9e35db1252e")
-	vote := OperVoteFund{PubKey: pubKey, VoteValue: 10000}
-	txParams.Votes.Add(&vote)
 
-	hash, err := SignDelegateTx(privateKey, &txParams)
-	if err != nil {
-		t.Error("SignDelegateTx err: ", err)
-	}
-	println(hash)
-}
 /*
-部署合约
-deploy contract
-fee Minimum 1.1 wicc
+	多币种部署合约,推荐使用
+	fee Minimum: 110000000 sawi
 */
-func TestSignRegisterContractTx(t *testing.T) {
-
-	privateKey := "YAa1wFCfFnZ5bt4hg9MDeDevTMd1Nu874Mn83hEXwtfAL2vkQE9f" //"Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
-
-	script, err := ioutil.ReadFile("./demo/data/hello.lua")
-	if err != nil {
-		t.Error("Read contract script file err: ", err)
-	}
-
-	var txParam RegisterContractTxParam
-	txParam.ValidHeight = 630314 //20999
-	txParam.SrcRegId = "0-1"     //"7849-1"
-	txParam.Fees = 110000000
-	txParam.Script = script
-	txParam.Description = "My hello contract!!!"
-	hash, err := SignRegisterContractTx(privateKey, &txParam)
-	if err != nil {
-		t.Error("SignRegisterContractTx err: ", err)
-	}
-	println(hash)
-}
-/*
-部署合约 多币种
-小费类型 WICC/WUSD 默认WICC
-fee Minimum 1.1 wicc/wusd
-*/
-func TestSignUCoinRegisterContractTx(t *testing.T) {
+func TestSignUCoinDeployContractTx(t *testing.T) {
 
 	privateKey := "YAa1wFCfFnZ5bt4hg9MDeDevTMd1Nu874Mn83hEXwtfAL2vkQE9f" //"Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
 
@@ -152,52 +85,107 @@ func TestSignUCoinRegisterContractTx(t *testing.T) {
 }
 
 /*
-  * 多币种转账交易 ,支持多种币种转账
-  * Test nUniversal Coin Transfer Tx
-  * fee Minimum 0.01 wicc
-  * */
-func TestSignUCoinTransferTx(t *testing.T) {
-	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
-	var txParam UCoinTransferTxParam
-	txParam.FeeSymbol = string(commons.WICC)
-	txParam.Fees = 1000000
-	txParam.ValidHeight = 297449
-	txParam.SrcRegId = "0-1"
-	txParam.Dests=NewDestArr()
-	dest:=Dest{string(commons.WICC),1000000, "wNDue1jHcgRSioSDL4o1AzXz3D72gCMkP6"}
-	txParam.Dests.Add(&dest)
-	txParam.PubKey = "036c5397f3227a1e209952829d249b7ad0f615e43b763ac15e3a6f52627a10df21"
-	txParam.Memo = ""
-	hash, err := SignUCoinTransferTx(privateKey, &txParam)
+   部署合约 ,不推荐
+   fee Minimum :110000000 sawi
+*/
+func TestSignDeployContractTx(t *testing.T) {
+
+	privateKey := "YAa1wFCfFnZ5bt4hg9MDeDevTMd1Nu874Mn83hEXwtfAL2vkQE9f" //"Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
+
+	script, err := ioutil.ReadFile("./demo/data/hello.lua")
 	if err != nil {
-		t.Error("UCoinTransferTx err: ", err)
+		t.Error("Read contract script file err: ", err)
+	}
+
+	var txParam RegisterContractTxParam
+	txParam.ValidHeight = 630314 //20999
+	txParam.SrcRegId = "0-1"     //"7849-1"
+	txParam.Fees = 110000000
+	txParam.Script = script
+	txParam.Description = "My hello contract!!!"
+	hash, err := SignRegisterContractTx(privateKey, &txParam)
+	if err != nil {
+		t.Error("SignRegisterContractTx err: ", err)
 	}
 	println(hash)
 }
 
 /*
-  * 多币种合约调用交易 ,支持多种币种转账
-  * Test nUniversal Coin Contract Tx
-  * fee Minimum 0.01 wicc
-  * */
-func TestSignUCoinContractTransferTx(t *testing.T) {
+Asset Create 创建资产
+
+fee Minimum 0.01WICC
+make sure account balance >550.01 wicc
+发布资产需要扣除550wicc的发布费用
+*/
+func TestAssetCreateTx(t *testing.T) {
 	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
-	var txParam UCoinContractTxParam
+	var txParam AssetIssueTxParam
 	txParam.FeeSymbol = string(commons.WICC)
-	txParam.CoinSymbol = string(commons.WICC)
-	txParam.CoinAmount = 1000000
 	txParam.Fees = 1000000
-	txParam.ValidHeight = 297449
+	txParam.ValidHeight = 11625
 	txParam.SrcRegId = "0-1"
-	txParam.AppId = "0-1"
-	txParam.PubKey = "036c5397f3227a1e209952829d249b7ad0f615e43b763ac15e3a6f52627a10df21"
-	txParam.ContractHex = "f017"
-	hash, err := SignUCoinCallContractTx(privateKey, &txParam)
+	txParam.AssetOwner="0-1"//only support regid
+	txParam.AssetTotal=100*100000000
+	txParam.AssetSymbol="SSSSSSS"
+	txParam.AssetName="SK Token"
+	txParam.MinTable=true
+	hash, err := SignAssetCreateTx(privateKey, &txParam)
 	if err != nil {
-		t.Error("SignUCoinCallContractTx err: ", err)
+		t.Error("SignAssetIssueTx err: ", err)
 	}
 	println(hash)
 }
+
+/*
+Asset Update 更新资产
+fee Minimum 0.01WICC
+make sure account balance >110.01 wicc
+发布资产需要扣除110wicc的发布费用
+*/
+func TestAssetUpdateTx(t *testing.T) {
+	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
+	var txParam AssetUpdateTxParam
+	txParam.FeeSymbol = string(commons.WICC)
+	txParam.Fees = 1000000
+	txParam.ValidHeight = 688709
+	txParam.SrcRegId = "0-1"
+	txParam.UpdateType=int(commons.ASSET_OWNER_UID)
+	txParam.AssetSymbol="STOKEN"
+	txParam.AssetOwner="111-1" //only support regid
+	//txParam.AssetTotal=100*100000000
+	//txParam.AssetName="SK Token"
+	hash, err := SignAssetUpdateTx(privateKey, &txParam)
+	if err != nil {
+		t.Error("AssetUpdateTx err: ", err)
+	}
+	println(hash)
+}
+
+
+/*
+* 投票交易
+* Voting transaction
+* fee Minimum 0.01 wicc
+* */
+func TestSignDelegateTx(t *testing.T) {
+	privateKey := "Y9sx4Y8sBAbWDAqAWytYuUnJige3ZPwKDZp1SCDqqRby1YMgRG9c"
+	var txParams DelegateTxParam
+	txParams.ValidHeight = 95728
+	txParams.SrcRegId = ""
+	txParams.Fees = 1000000
+	txParams.PubKey = "03e93e7d870ce6f1c9997076c56fc24e6381c612662cd9a5a59294fac9ba7d21d7"
+	txParams.Votes = NewOperVoteFunds()
+	pubKey, _ := hex.DecodeString("025a37cb6ec9f63bb17e562865e006f0bafa9afbd8a846bd87fc8ff9e35db1252e")
+	vote := OperVoteFund{PubKey: pubKey, VoteValue: 10000}
+	txParams.Votes.Add(&vote)
+
+	hash, err := SignDelegateTx(privateKey, &txParams)
+	if err != nil {
+		t.Error("SignDelegateTx err: ", err)
+	}
+	println(hash)
+}
+
 
 /*
     * 创建,追加cdp交易
@@ -400,53 +388,3 @@ func TestSignDexCancelTx(t *testing.T) {
 	println(hash)
 }
 
-/*
-Asset Create 创建资产
-
-fee Minimum 0.01WICC
-make sure account balance >550.01 wicc
-发布资产需要扣除550wicc的发布费用
-*/
-func TestAssetCreateTx(t *testing.T) {
-	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
-	var txParam AssetIssueTxParam
-	txParam.FeeSymbol = string(commons.WICC)
-	txParam.Fees = 1000000
-	txParam.ValidHeight = 11625
-	txParam.SrcRegId = "0-1"
-	txParam.AssetOwner="0-1"//only support regid
-	txParam.AssetTotal=100*100000000
-	txParam.AssetSymbol="SSSSSSS"
-	txParam.AssetName="SK Token"
-	txParam.MinTable=true
-	hash, err := SignAssetCreateTx(privateKey, &txParam)
-	if err != nil {
-		t.Error("SignAssetIssueTx err: ", err)
-	}
-	println(hash)
-}
-
-/*
-Asset Update 更新资产
-fee Minimum 0.01WICC
-make sure account balance >110.01 wicc
-发布资产需要扣除110wicc的发布费用
-*/
-func TestAssetUpdateTx(t *testing.T) {
-	privateKey := "Y6J4aK6Wcs4A3Ex4HXdfjJ6ZsHpNZfjaS4B9w7xqEnmFEYMqQd13"
-	var txParam AssetUpdateTxParam
-	txParam.FeeSymbol = string(commons.WICC)
-	txParam.Fees = 1000000
-	txParam.ValidHeight = 688709
-	txParam.SrcRegId = "0-1"
-	txParam.UpdateType=int(commons.ASSET_OWNER_UID)
-	txParam.AssetSymbol="STOKEN"
-	txParam.AssetOwner="111-1" //only support regid
-	//txParam.AssetTotal=100*100000000
-	//txParam.AssetName="SK Token"
-	hash, err := SignAssetUpdateTx(privateKey, &txParam)
-	if err != nil {
-		t.Error("AssetUpdateTx err: ", err)
-	}
-	println(hash)
-}
